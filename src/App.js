220e204e52a948ca43217357985e31de5364e778
EMMANUEL\Emmanuel <esiphuma@solugrowth.com>
2021-03-15 19:35:32 +0200

    my react appp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Signin from './attribute/Signin';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Signin/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/attribute/Signin.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Signin extends Component {
+  render() {
+    return (
+      <div className="Signin"> 
+      <h1>welcome to my page</h1>
+      <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        </div>
+    );
+  }
+}
+
